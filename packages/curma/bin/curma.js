36b23540c24b8471fd691a8b99245886b2f5cb41
DY_XiaoDong <xiaodong@indouyin.cn>
2023-01-28 19:53:02 +0800

    Update curma - cac

--- packages/curma/bin/curma.js
+++ packages/curma/bin/curma.js
@@ -1,16 +1,28 @@
-import curma from "commander";
+#! /usr/bin/env node
+
+import cli from "cac"
+import dev from "../dist/dev/index.js";
+import {createRequire} from "node:module";
+
+const require = createRequire(import.meta.url);
+const pkg = require("../package.json");
+const curma = cli("curma");
 
 curma
-  .name("curma")
-  .description("Curma is a esm dev build tool.")
-  .version(`curma ${require("../package.json").version}`)
-  .usage("<command> [options]");
+  .version(pkg.version)
+  .option("-h, --help", "output help information")
+  .command("[root]", "Output help information")
+  .action(() => {
+    curma.outputHelp();
+  })
 
 curma
-  .command("dev")
-  .description("start a dev server")
+  .command("dev", "start a dev server")
+  // .description("start a dev server")
   .option("-p, --port <port>", "set dev server port")
   .action((cmd) => {
     // require start fn and run
-    require("../dist/dev.js")(cmd);
+    dev(cmd);
   });
+
+curma.parse();
