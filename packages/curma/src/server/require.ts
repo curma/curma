fe50bc62266edae6a0e92bb3254a3b0a6ef578c6
DY_XiaoDong <xiaodong@indouyin.cn>
2023-01-31 19:29:14 +0800

    Update curma - src/server/require

---
+++ packages/curma/src/server/require.ts
@@ -0,0 +1,37 @@
+type fileIndex = string;
+
+export default function require(fileIndex: string, url: string): fileIndex {
+    // Find require
+    const requireRegex: RegExp = /require\((['"])(.*?)\1\)/g;
+    const requires: RegExpMatchArray = fileIndex.match(requireRegex) || [];
+    for (let require of requires) {
+        const requirePath: string = require.replace(/require\((['"])(.*?)\1\)/g, "$2");
+        fileIndex
+            .replace(
+                require, `window.require("${url}", "${requirePath}")`
+            )
+    }
+
+    // if using module.exports for export
+    if (fileIndex.includes("module.exports")) {
+        // use curma_module_exports
+        // if there is a 'use strict' at the top of the file
+        if (fileIndex.startsWith("'use strict'")) {
+            fileIndex = fileIndex.replace("'use strict'", "");
+        }
+        fileIndex = "let curma_module_exports; " + fileIndex;
+        // give val -> curma_module_exports
+        fileIndex = fileIndex.replace("module.exports", "curma_module_exports");
+        fileIndex += "; export default curma_module_exports;";
+        // if it is import by other esm
+        // req.url -> /...&import=item1,item2
+        if (url.includes("&import=")) {
+            const imports: string[] = url.split("&import=")[1].split(",");
+            for (let item of imports) {
+                fileIndex += `export const ${item} = curma_module_exports.${item};`;
+            }
+        }
+    }
+
+    return fileIndex;
+}
