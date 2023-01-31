d1170820a50a1384eb6812f291a73ccf8bfad7d8
DY_XiaoDong <xiaodong@indouyin.cn>
2023-01-31 12:15:20 +0800

    Update curma - playground

--- packages/curma/playground/main.js
+++ packages/curma/playground/main.js
@@ -1,23 +1,29 @@
-import {selecter} from "fastjs-next";
-import "./style.css";
+// import {selecter} from "fastjs-next";
+// import "./style.css";
+//
+// selecter("#app").html(`
+//   <h1>Hello Curma!</h1>
+//   <div class="card">
+//       <button id="counter" type="button">count is 0</button>
+//   </div>
+//   <div class="select">
+//     <span>Curma&nbsp;|</span>
+//     <a href="https://github.com/curma/curma/" target="_blank">Github</a>
+//   </div>
+// `)
+//
+// // mount click event
+// let time = 0;
+// let el = selecter("#counter");
+// el.on("click", () => {
+//   el.html(`count is ${++time}`);
+// })
 
-const json = require("./test.json")
-console.log(json)
+// import {createApp, ref} from 'vue'
+import {createApp} from 'vue'
+import App from './App.vue'
+// import './style.css'
 
-selecter("#app").html(`
-  <h1>Hello Curma!</h1>
-  <div class="card">
-      <button id="counter" type="button">count is 0</button>
-  </div>
-  <div class="select">
-    <span>Curma&nbsp;|</span>
-    <a href="https://github.com/curma/curma/" target="_blank">Github</a>
-  </div>
-`)
+const app = createApp(App)
 
-// mount click event
-let time = 0;
-let el = selecter("#counter");
-el.on("click", () => {
-  el.html(`count is ${++time}`);
-})
+app.mount('#app')
