a9b270c7cbfd2f992aae0c8717f563e25185f619
DY_XiaoDong <xiaodong@indouyin.cn>
2023-01-31 19:39:32 +0800

    Update curma - src/server

--- packages/curma/src/server/index.ts
+++ packages/curma/src/server/index.ts
@@ -4,6 +4,7 @@ import {createRequire} from "node:module";
 import {WebSocketServer} from 'ws';
 import type {WebSocket} from 'ws';
 import chokidar from 'chokidar';
+import type {curmaConfig} from '../curma';
 
 import output from '../console/output.js';
 import outputDev from '../console/outputDev.js'
@@ -20,12 +21,16 @@ const portfinder = require('portfinder')
 const pkg: { [key: string]: any } = require('../../package.json');
 const usr_pkg: { [key: string]: any } = require(path.join(process.cwd(), 'package.json'));
 
+import curma_require from "./require";
+
 export default async function () {
     // start time
     const startTime = Date.now();
     output("")
     const _loader = new loader("Starting server", "Loading config", 6);
-    const _config = eval(`Object(${fs.readFileSync(path.join(process.cwd(), 'curma.config.js'), 'utf-8').match(/export default (.*)/s)[0].match(/\{(.*)}/s)[0]})`);
+    let _config: curmaConfig = {}
+    if (fs.existsSync(path.join(process.cwd(), 'curma.config.js')))
+        _config = eval(`Object(${fs.readFileSync(path.join(process.cwd(), 'curma.config.js'), 'utf-8').match(/export default (.*)/s)[0].match(/\{(.*)}/s)[0]})`);
     outputDev("config:", _config);
     // check root
     if (!_config.root) _config.root = "/";
@@ -51,15 +56,21 @@ export default async function () {
             if (!fs.existsSync(file) || !fs.statSync(file).isFile()) {
                 // try auto index
                 for (let index of _autoIndex) {
-                    if (fs.existsSync(file + index)) {
-                        return file + index;
+                    if (index[0] === ".") {
+                        if (fs.existsSync(file + index)) {
+                            return file + index;
+                        }
+                    } else {
+                        if (fs.existsSync(path.join(file, index))) {
+                            return path.join(file, index);
+                        }
                     }
                 }
                 return false
             }
             return file
         }
-        const fileUrl = _isExists(path.join(process.cwd(), req.url || ""))
+        const fileUrl = _isExists(path.join(process.cwd(), req.url || "")) || _isExists(path.join(process.cwd(), "public", req.url || ""));
         let fileIndex = "";
         if (fileUrl) fileIndex = fs.readFileSync(fileUrl).toString();
         const _contentTypeMap = {
@@ -86,10 +97,38 @@ export default async function () {
             return fileExt;
         }
         const _fileExt = setContentType(fileUrl || "")
+        if (req.url?.includes(`/curma-package`)) {
+            let packageName = req.url?.split("curma-package?name=")[1];
+            packageName = packageName?.split("&")[0];
+            if (packageName.includes("/")) {
+                const _path = path.join(process.cwd(), "node_modules", packageName);
+                if (fs.existsSync(_path)) {
+                    fileIndex = fs.readFileSync(_path).toString();
+                } else warn(`File not found: ${_path}`);
+            } else if (packageName) {
+                outputDev("Request package - " + packageName);
+                const packageInfo = require(path.join(process.cwd(), 'node_modules', packageName, 'package.json'));
+                if (packageName === "vue") {
+                    packageInfo.module = "dist/vue.esm-bundler.js";
+                }
+                output(path.join(process.cwd(), 'node_modules', packageName, packageInfo.module || packageInfo.main));
+                // return package.main
+                const mainFile = _isExists(path.join(process.cwd(), 'node_modules', packageName, packageInfo.module || packageInfo.main));
+                if (!mainFile) {
+                    warn(`package ${packageName} has no main file`);
+                } else {
+                    if (!fs.existsSync(mainFile)) warn(`package ${packageName} main file not found: ${packageInfo.main}`);
+                    else {
+                        fileIndex = fs.readFileSync(mainFile).toString();
+                        res.setHeader("Content-Type", "application/javascript");
+                    }
+                }
+            }
+        }
         if (_fileExt === "html") {
-            const _curmaScriptIndex = String(fs.readFileSync(path.join(__dirname, "../src/server/scripts/dev.js")))
+            const _curmaScriptIndex = String(fs.readFileSync(path.join(__dirname, "node_modules/curma/dist/server/scripts/dev.js")))
                 .replace("?port?", port)
-            fileIndex = `<script>${_curmaScriptIndex}</script>${fileIndex}`
+            fileIndex = fileIndex.replace("</head>", `<script>${_curmaScriptIndex}</script></head>`)
         }
         if (_fileExt === "css") {
             // if Sec-Fetch-Dest is script
@@ -99,33 +138,43 @@ export default async function () {
             }
         }
         if (_fileExt === "js" || _fileExt === "vue" || _fileExt === "ts") {
-            const _cdnDependencies = fileIndex.match(/import .*? from (['"])(?:@([^/]+?)\/)?([^/]+?)(['"])/g) || [];
-            for (const _cdnDependency of _cdnDependencies) {
+        }
+        // @/ -> /src
+        const _alias = fileIndex.match(/import\s+.*\s+from\s+['"]@\/(.*)['"]/g);
+        if (_alias) {
+            for (let i of _alias) {
                 // @ts-ignore
-                const dependName = _cdnDependency.match(/(["'])(?:@([^/]+?)\/)?([^/]+?["'])/g)[0].replace(/["']/g, "");
-                // if config.cdnDependencies.dev
-                if (_config.cdnDependencies.dev) {
-                    // check is dependency in package.json
-                    let version = "";
-                    if (dependName in usr_pkg.dependencies) {
-                        // version
-                        version = `@${usr_pkg.dependencies[dependName]}`
-                    } else _config.error.cdnDependenciesNoVersion && warn(`Dependency ${dependName} not found in package.json, using latest version. [curma:cdnDependenciesNoVersion]`);
-                    fileIndex = fileIndex.toString().replace(_cdnDependency
-                        , _cdnDependency.replace(dependName, `https://cdn.jsdelivr.net/npm/${dependName}${version}/+esm`));
-                } else {
-                    let replace = `${_config.root}curma-package?name=${dependName}`;
-                    // if import {xxx}
-                    if (_cdnDependency.includes("{")) {
-                        replace = `${_config.root}curma-package?name=${dependName}&import=${(_cdnDependency.match(/{(.*)}/) || [])[1].toString().replace(/ /g, "")}`
-                    }
-                    fileIndex = fileIndex.toString().replace(_cdnDependency
-                        , _cdnDependency.replace(dependName, replace));
-                }
+                const _aliasPath = i.match(/import\s+.*\s+from\s+['"]@\/(.*)['"]/)[1];
+                fileIndex = fileIndex.replace(i, i.replace("@\/", `${_config.root}src/`));
             }
-            // check require
-            if (/require\((['"])(.*)\1\)/.test(fileIndex)) {
-                // fileIndex = "import {createRequire} from \"node:module\";const require = createRequire(import.meta.url);\n".concat(fileIndex);
+        }
+        const _cdnDependencies = fileIndex.match(/(from|import) ['"][a-z@].+?['"]/g) || [];
+        for (const _cdnDependency of _cdnDependencies) {
+            // @ts-ignore
+            const dependName = _cdnDependency.match(/["'][a-z@].+?["']/g)[0].replace(/["']/g, "");
+            // if config.cdnDependencies.dev
+            if (_config.cdnDependencies?.dev) {
+                // check is dependency in package.json
+                let version = "";
+                if (dependName in (usr_pkg.dependencies || {})) {
+                    // version
+                    version = `@${usr_pkg.dependencies[dependName]}`
+                } else _config.error?.cdnDependenciesNoVersion && warn(`Dependency ${dependName} not found in package.json, using latest version. [curma:cdnDependenciesNoVersion]`);
+                if (_config.cdnDependencies?.custom && dependName in _config.cdnDependencies.custom)
+                    fileIndex = fileIndex.replace(_cdnDependency
+                        , _cdnDependency.replace(dependName, _config.cdnDependencies.custom[dependName]
+                            .replace(/{version}/g, version)));
+                else
+                    fileIndex = fileIndex.replace(_cdnDependency
+                        , _cdnDependency.replace(dependName, `https://cdn.jsdelivr.net/npm/${dependName}${version}/+esm`));
+            } else {
+                let replace = `${_config.root}curma-package?name=${dependName}`;
+                // if import {xxx}
+                if (_cdnDependency.includes("{")) {
+                    replace = `${_config.root}curma-package?name=${dependName}&import=${(_cdnDependency.match(/{(.*)}/) || [])[1].toString().replace(/ /g, "")}`
+                }
+                fileIndex = fileIndex.toString().replace(_cdnDependency
+                    , _cdnDependency.replace(dependName, replace));
             }
         }
         if (_fileExt === "vue") {
@@ -139,30 +188,60 @@ export default async function () {
             let template = templateMatch?.[0].replace(/<template>([\s\S]*)<\/template>/g, "$1") || "";
             let script = scriptMatch?.[0].replace(/<script>([\s\S]*)<\/script>/g, "$1") || "";
             res.setHeader("Content-Type", "application/javascript");
+            // abc;export default {...} -> abc;
+            let scriptTop = script.replace(/export default\s+{[\s\S]*}/g, "");
             // export default {...} -> {...}
-            script = script.replace(/export default /g, "").match(/{([\s\S]*)}/g)?.[0] || "";
+            // script = script.replace(/export default +/g, "").match(/{([\s\S]*)}/g)?.[0] || "";
+            script = script.match(/export default +{([\s\S]*)}/g)?.[0].replace(/export default +/g, "") || "";
             // template去除右侧空格
             template = template.replace(/>\s+</g, "><");
-            fileIndex = `export default {template: \`${template}\`, style: \`${style?.[0] || ""}\`, ${script.substring(1)}`;
+            fileIndex = `${scriptTop}
+            
+            export default {template: \`${template}\`, style: \`${style?.[0] || ""}\`, ${script.substring(1)}`;
         }
-        if (req.url?.includes(`${_config.root}curma-package`)) {
-            let packageName = req.url?.split("curma-package?name=")[1];
-            packageName = packageName?.split("&")[0];
-            if (packageName) {
-                outputDev("Request package - " + packageName);
-                const packageInfo = require(path.join(process.cwd(), 'node_modules', packageName, 'package.json'));
-                // return package.main
-                const mainFile = _isExists(path.join(process.cwd(), 'node_modules', packageName, packageInfo.main));
-                if (!mainFile) {
-                    warn(`package ${packageName} has no main file`);
+        if (req.url?.includes(`/curma-require`)) {
+            let requireName = req.url?.split("name=").slice(1).join("name=")
+
+            const pathList = requireName.split("&&").map((item: string, key: number) => {
+                outputDev(`requireName: ${item}`);
+                // if curma-package
+                if (item.includes("curma-package")) {
+                    output(`curma-package: ${item} =============`);
+                    const packageName = item.split("curma-package?name=")[1].split("&")[0];
+                    return path.join(
+                        "node_modules",
+                        packageName,
+                        require(path.join(process.cwd(), 'node_modules', packageName, 'package.json')).main.replace(/[a-zA-Z0-9_\-]+\.[a-z]+$/, "")
+                    );
+                }
+                if (item[0] === "/") item = item.substring(1);
+                // item = a/b/c  ->  if c have no . -> a/b
+                // item = "a/b/c"  ->  if c have no . -> item = "a/b"
+                if (!item.includes(".")) {
+                    console.log(item);
+                    // get c
+                    const c = item.split("/").pop();
+                    // if c is not a dict
+                    if (!fs.existsSync(path.join(process.cwd(), item, c))) {
+                        return item.split("/").slice(0, -1).join("/");
+                    }
                 } else {
-                    if (!fs.existsSync(mainFile)) warn(`package ${packageName} main file not found: ${packageInfo.main}`);
-                    else {
-                        fileIndex = fs.readFileSync(mainFile).toString();
-                        res.setHeader("Content-Type", "application/javascript");
+                    // is last item?
+                    if (!(key === requireName.split("&&").length - 1)) {
+                        return item.split("/").slice(0, -1).join("/");
                     }
                 }
+                return item;
+            });
+            const _path = path.resolve(process.cwd(), ...pathList)
+            let _require = "export default {}";
+            output(`require: ${_path}`);
+            if (fs.existsSync(_path)) {
+                _require = fs.readFileSync(_path).toString();
+            } else {
+                warn("Require file not found: " + _path, "[curma:requireFileNotFound]");
             }
+            fileIndex = _require;
         }
         // get file
         if (!fileUrl && !fileIndex) {
@@ -170,21 +249,7 @@ export default async function () {
             res.end("404 Not Found");
             return;
         }
-        // all file check
-        fileIndex = fileIndex
-            .replace(/process\.env\.NODE_ENV/g, `"development"`)
-            .replace(/require\(/g, `await window.require('${req.url}&&'+`)
-        if (fileIndex.includes("module.exports")) {
-            fileIndex = fileIndex.replace(/module\.exports/g, `curma_module_exports`)
-            fileIndex += `;export default curma_module_exports;`
-            // check &import=xxx
-            if (req.url?.includes("&import=")) {
-                const importList = req.url?.split("&import=")[1].split(",");
-                for (let importItem of importList) {
-                    fileIndex += `export let ${importItem} = curma_module_exports.${importItem};`
-                }
-            }
-        }
+        fileIndex = curma_require(fileIndex, req.url || "");
         res.writeHead(200);
         res.end(fileIndex);
     })
@@ -196,30 +261,6 @@ export default async function () {
     const _wsClients: Array<WebSocket> = [];
     ws.on('connection', function connection(ws) {
         _wsClients.push(ws);
-        ws.on('message', function incoming(message) {
-            let msg: {[key: string]: any} = JSON.parse(message.toString());
-            if (msg.type === "require") {
-                const pathList = msg.name.split("&&").map((item: string) => {
-                    // if curma-package
-                    if (item.includes("curma-package")) {
-                        const packageName = item.split("curma-package?name=")[1].split("&")[0];
-                        return path.join(
-                            "node_modules",
-                            packageName,
-                            require(path.join(process.cwd(), 'node_modules', packageName, 'package.json')).main.replace(/[a-zA-Z0-9_\-]+\.[a-z]+$/, "")
-                        );
-                    }
-                    return item;
-                });
-                output("Require: " + pathList.join(", "));
-                const _require = fs.readFileSync(path.join(process.cwd(), ...pathList)).toString();
-                ws.send(JSON.stringify({
-                    type: "require",
-                    data: _require,
-                    id: msg.id
-                }));
-            }
-        })
     })
     const reload = () => {
         for (let client of _wsClients) {
